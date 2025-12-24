a9ad35bd2face5d11d14a9c943d3af3673ad9200
unknown <sameeksha.tantak@cumminscollege.in>
2025-12-24 15:54:42 +0530

    changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,10 @@
 import "../style/Home.css";
 import AboutUs from "./AboutUs";
-import DevTeam from "./DevTeam";
 import Event from "./Event";
 import Footer from "./Footer";
 import GandhaarTheme from "./GandhaarTheme";
 import Header from "./Header";
 import ImageGallery from "./ImageGallery";
-import Panel from "./Panel";
 import Schedule from "./Schedule";
 import StarLineUp from "./StarLineUp";
 
@@ -20,13 +18,11 @@ function Home() {
       <Schedule/>
       <AboutUs/>
       <ImageGallery/>
-      <Panel/>
-      <DevTeam/>
       <br></br>
       <br></br>
       <br></br>
       <br></br>
-     <Footer/>
+      <Footer/>
     </div>
   );
 }
