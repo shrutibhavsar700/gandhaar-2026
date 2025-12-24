c6b442c9762184dc7d24d4e24ec76098c4cb1280
unknown <sameeksha.tantak@cumminscollege.in>
2025-12-24 17:12:12 +0530

    Revert "temp3"
    This reverts commit de93af3e2444fe72a3397c0789720f5115fcc3a0.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,10 @@
-import "../style/Home.css";
-import AboutUs from "./AboutUs";
-import Event from "./Event";
 import Footer from "./Footer";
-import GandhaarTheme from "./GandhaarTheme";
-import Header from "./Header";
-import ImageGallery from "./ImageGallery";
-import Schedule from "./Schedule";
-import StarLineUp from "./StarLineUp";
+import Theme from "./Theme";
 
 function Home() {
   return (
     <div>
-      <Header/>
-      <GandhaarTheme/>
-      <Event/>
-      <StarLineUp/>
-      <Schedule/>
-      <AboutUs/>
-      <ImageGallery/>
-      <br></br>
-      <br></br>
-      <br></br>
+      <Theme />
       <br></br>
       <Footer/>
     </div>
